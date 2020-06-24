1ca0a86424cede27bebf31ab1c8d8f2ff0fa50e4
btskinner <btskinner@coe.ufl.edu>
2020-06-24 15:12:44 -0400

    changing branch name from master to main

--- _publishwebsite.sh
+++ _publishwebsite.sh
@@ -2,8 +2,8 @@
 
 export FILTER_BRANCH_SQUELCH_WARNING=1
 
-git branch -D master
-git checkout -b master
+git branch -D main
+git checkout -b main
 git filter-branch --subdirectory-filter _site/ -f
 git reflog expire --expire=now --all
 git gc --prune=now
